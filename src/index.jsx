eb1c59fb63ddb400d0b54ca7b1bf250919b6eed0
Iva Havranová <havranova.i@seznam.cz>
2023-07-02 00:26:46 +0200

    color states created

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import { ColorSlider } from './components/ColorSlider';
-
+// Opět uvnitř komponenty App vytvořte tři stavy redValue, greenValue, blueValue. Každý reprezentuje hodnotu jedné z barevných složek. Použijte tyto hodnoty k vytvoření finální barvy uvnitř prvku .color-box
 const App = () => {
+  const [redValue, setRedValue] = useState('');
+  const [greenValue, setGreenValue] = useState('');
+  const [blueValue, setBlueValue] = useState('');
   return (
     <div className="color-panel">
       <h1>Mixér barev</h1>
